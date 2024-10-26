2468ae36911514a9f2febec36ae23a47fde58142
TacticalVoteScotland <robin@robinwilde.me>
2024-10-25 21:21:44 -0700

    Logic gate on form

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ for (i = 0; i < acc.length; i++) {
   });
 }
 
+const confirmation = document.getElementById("confirmation");
 const submitButton = document.getElementById("submitButton");
 const rsvpForm = document.getElementById("rsvpForm");
 const formResponse = document.getElementById("formResponse");
@@ -33,6 +34,25 @@ rsvpForm.addEventListener("submit", function(e) {
 submitButton.addEventListener("click", function() {
     rsvpForm.style.display = "none";
     formResponse.style.display = "block";
+  }
+)
+
+confirmation.addEventListener("change", function() {
+    if (confirmation.value == "Yes") {
+      document.querySelector('#origin').style.display = "block";
+      document.querySelector('#dietary').style.display = "block";
+      document.querySelector('#access').style.display = "block";
+      document.querySelector('#family').style.display = "block";
+      document.querySelector('#kids').style.display = "block";
+      document.querySelector('#volunteer').style.display = "block";
+    } else {
+      document.querySelector('#origin').style.display = "none";
+      document.querySelector('#dietary').style.display = "none";
+      document.querySelector('#access').style.display = "none";
+      document.querySelector('#family').style.display = "none";
+      document.querySelector('#kids').style.display = "none";
+      document.querySelector('#volunteer').style.display = "none";
+    }
   }
 )
 
