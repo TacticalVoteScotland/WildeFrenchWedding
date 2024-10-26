8249ccbeb4f91b5e512c900e66a5fbc5a887c371
TacticalVoteScotland <robin@robinwilde.me>
2024-10-25 20:33:38 -0700

    first deploy

---
+++ script.js
@@ -0,0 +1,26 @@
+let acc = document.getElementsByClassName("questionHeader");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+      panel.style.margin = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+      panel.style.marginBottom = "20px";
+    }
+  });
+}
+
+const submitButton = document.getElementById("submitButton");
+const rsvpForm = document.getElementById("rsvpForm");
+const formResponse = document.getElementById("formResponse");
+
+submitButton.addEventListener("click", function() {
+    rsvpForm.style.display = "none";
+    formResponse.style.display = "block";
+  }
+)
